a350a875cdf083bd479fbb75553100a1415992b8
Ann-Gem <anngem0101@gmail.com>
2021-07-25 00:12:34 +0300

    header_and_animation

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-let tab= document.getElementById('a01');
-tab.addEventListener("click", function() {
-  this.classList.add("active");
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+  optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach(option => {
+  option.addEventListener("click", () => {
+    selected.innerHTML = option.querySelector("label").innerHTML;
+    optionsContainer.classList.remove("active");
+  });
 });
